7c42959f81ec45fc5532ea171d89c3254749df7c
KyaniBrito <kyanimag@icloud.com>
2025-11-07 17:48:50 -0300

    ajusta ordem dos cards

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -2,6 +2,7 @@ const Card = require("../models/card");
 
 module.exports.getCards = (req, res, next) => {
   Card.find({})
+    .sort({ createdAt: -1 })
     .then((cards) => res.send(cards))
     .catch(next);
 };
